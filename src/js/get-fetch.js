5bd51fc611eb4b19e69c35cfb708afc17c56d8a7
iamfredric <fredricfarholt@live.se>
2019-10-13 15:41:56 +0200

    commit

--- src/js/get-fetch.js
+++ src/js/get-fetch.js
@@ -1,19 +1,104 @@
 function displayData(x0) {
-  console.log(x0);
+  let element = document.getElementById("box-data");
+  element.innerHTML = "";
+  for (let i = 0; i < x0.length; i++) {
+    element.innerHTML +=
+      "<form class='form'><div class='content_container'>" +
+      "<p class='p1'><input id='i1' type='text' name='courseCode' value='" +
+      x0[i].courseCode +
+      "'></p>" +
+      "<p class='p1'><input type='text' name='courseProgression' value='" +
+      x0[i].progression +
+      "'></p>" +
+      "<p class='p2'><input type='text' name='courseName' value='" +
+      x0[i].courseName +
+      "'></p>" +
+      "<p class='p2'><input type='text' name='courseName' value='" +
+      x0[i].syllabus +
+      "'></p>" +
+      "<button onClick='deleteData(" +
+      x0[i].id +
+      ")' class='__delete' name='courseId' type='button'>Ta bort</button>" +
+      "<button class='__save' type='submit'>Spara</button>" +
+      "</div></form>";
+  }
+
+  loadForm();
 }
 
 const url =
   "http://localhost/DT173G%20-%20Moment%205/api/app/ControllerView/CourseView.php";
-fetch(url)
-  .then(function(response) {
-    if (response.status !== 200) {
-      console.log(response.status);
-      return;
-    }
-    response.json().then(function(data) {
-      displayData(displayData);
-    });
+
+let getData = () => {
+  fetch(url, {
+    method: "GET"
   })
-  .catch(function(err) {
-    console.log("Fetch Error:", err);
-  });
+    .then(function(response) {
+      if (response.status !== 200) {
+        console.log(response.status);
+        return;
+      }
+      response.json().then(function(data) {
+        displayData(data);
+        console.log(data);
+      });
+    })
+    .catch(function(err) {
+      console.log("Fetch Error:", err);
+    });
+};
+
+getData();
+
+let deleteData = id => {
+  fetch(url, {
+    method: "DELETE",
+    body: JSON.stringify({ id: id })
+  })
+    .then(function(response) {
+      if (response.status !== 200) {
+        console.log(response.status);
+        return;
+      }
+      response.json().then(function(data) {
+        getData();
+        console.log(data);
+      });
+    })
+    .catch(function(err) {
+      console.log("Fetch Error:", err);
+    });
+};
+
+let loadForm = () => {
+  let form = document.getElementsByClassName("form");
+
+  for (let i = 0; i < form.length; i++) {
+    // console.log(form[i]);
+    form[i].addEventListener("submit", function(e) {
+      e.preventDefault();
+
+      let a = $(this)
+        .find("input[name='courseName']")
+        .val();
+      console.log(a);
+
+      fetch(url, {
+        method: "PUT"
+      })
+        .then(function(response) {
+          if (response.status !== 200) {
+            console.log(response.status);
+            return;
+          }
+          response.json().then(function(data) {
+            getData();
+            console.log(data);
+          });
+        })
+        .catch(function(err) {
+          console.log("Fetch Error:", err);
+        });
+    });
+  }
+};
